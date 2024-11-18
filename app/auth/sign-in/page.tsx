17257fae34ad1e0e8ab7a2492567453da6ea74d1
zerobbreak <uthacker244@outlook.com>
2024-11-18 23:07:23 +0200

    Removal of auth files

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -1,9 +1,7 @@
-import { LoginForm } from "@/components/shared/loginForm"
 
 export default function Page() {
   return (
     <div className="flex h-screen w-full items-center justify-center px-4">
-      <LoginForm/>
     </div>
   )
 }
